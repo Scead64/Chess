590d2be7629df86e9d3c78afd1dfbbbd30a2f2bf
Scead64 <tinor.jain@gmail.com>
2024-02-15 14:11:16 -0500

    Added Board and Square classes to model

---
+++ src/main/java/com/chess/models/Board.java
@@ -0,0 +1,22 @@
+package com.chess.models;
+
+public class Board {
+    
+    Square[] squares;
+
+    public Board(){
+        squares = new Square[63];
+        for(int i = 0; i < squares.length; i++){
+            squares[i] = new Square();
+        }
+    }
+
+    /*
+     * @returns the piece located at the given location
+     * @requires the location given is with 0 <= location <= 63
+     */
+    public Piece getPiece(int location){
+        return squares[location].p;
+    }
+
+}
---
+++ src/main/java/com/chess/models/Square.java
@@ -0,0 +1,12 @@
+package com.chess.models;
+
+import javafx.scene.shape.Rectangle;
+
+public class Square {
+    Piece p;
+    Rectangle sq;
+
+    public Square(){
+    }
+
+}
