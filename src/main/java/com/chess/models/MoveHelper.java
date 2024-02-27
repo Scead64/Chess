b7f75e39231accb0f5b44bbe287d96b4376ddca6
Scead64 <tinor.jain@gmail.com>
2024-02-26 21:44:07 -0500

    Refactored Ordinal direction MoveHelper - Changed getCardinalDirection to getDirection - Removed deprecated code - Updated getOrdinalDirection

--- src/main/java/com/chess/models/MoveHelper.java
+++ src/main/java/com/chess/models/MoveHelper.java
@@ -7,27 +7,36 @@ public class MoveHelper{
     public static final int SOUTH = 8;
     public static final int EAST = 1;
     public static final int WEST = -1;
+
+    public static final int NORTH_EAST = -7;
+    public static final int NORTH_WEST = -9;
+    public static final int SOUTH_EAST = 9;
+    public static final int SOUTH_WEST = 7;
+
     
     public static ArrayList<Integer> getCardinals(int location, String color, Board board){
-        int maxNorth = location/8, maxEast = 7 - (location % 8),  maxSouth = 7-(location/8), maxWest = location % 8;
         ArrayList<Integer> moves = new ArrayList<Integer>();
-        moves.addAll(getCardinalDirection(location, NORTH, maxNorth, color, board));
-        moves.addAll(getCardinalDirection(location, EAST, maxEast, color, board));
-        moves.addAll(getCardinalDirection(location, SOUTH, maxSouth, color, board));
-        moves.addAll(getCardinalDirection(location, WEST, maxWest, color, board));
+        int maxNorth = location/8, maxEast = 7 - (location % 8),  maxSouth = 7-(location/8), maxWest = location % 8;
+
+        moves.addAll(getDirection(location, NORTH, maxNorth, color, board));
+        moves.addAll(getDirection(location, EAST, maxEast, color, board));
+        moves.addAll(getDirection(location, SOUTH, maxSouth, color, board));
+        moves.addAll(getDirection(location, WEST, maxWest, color, board));
         return moves;
     }
 
     public static ArrayList<Integer> getOrdinals(int location, String color, Board board){
         ArrayList<Integer> moves = new ArrayList<Integer>();
-        moves.addAll(getNorthEast(location, color, board));
-        moves.addAll(getNorthWest(location, color, board));
-        moves.addAll(getSouthEast(location, color, board));
-        moves.addAll(getSouthWest(location, color, board));
+        int maxNorth = location/8, maxEast = 7 - (location % 8),  maxSouth = 7-(location/8), maxWest = location % 8;
+
+        moves.addAll(getDirection(location, NORTH_EAST, Math.min(maxNorth, maxEast), color, board));
+        moves.addAll(getDirection(location, NORTH_WEST, Math.min(maxNorth, maxWest), color, board));
+        moves.addAll(getDirection(location, SOUTH_EAST, Math.min(maxSouth, maxEast), color, board));
+        moves.addAll(getDirection(location, SOUTH_WEST, Math.min(maxSouth, maxWest), color, board));
         return moves;
     }
     
-    public static ArrayList<Integer> getCardinalDirection(int location, int direction, int maxDistance, String color, Board board){
+    public static ArrayList<Integer> getDirection(int location, int direction, int maxDistance, String color, Board board){
         ArrayList<Integer> moves = new ArrayList<Integer>();
         for(int i = 1; i <= maxDistance; i++){
             if(verifyColor(moves, color, location + (i*direction), board.getSquare(location + (i*direction)))){
@@ -36,56 +45,7 @@ public class MoveHelper{
         }
         return moves;
     }
-
-    public static ArrayList<Integer> getNorthEast(int location, String color, Board board){
-        ArrayList<Integer> moves = new ArrayList<Integer>();
-        int maxNorth = location/8, maxEast = 7 - (location % 8);
-        int distance = Math.min(maxNorth, maxEast);
-        for(int i = 1; i <= distance; i++){
-            if(verifyColor(moves, color, location - (i*7), board.getSquare(location - (i*7)))){
-                break;
-            }
-        }
-        return moves;
-    }
-
-    public static ArrayList<Integer> getNorthWest(int location, String color, Board board){
-        ArrayList<Integer> moves = new ArrayList<Integer>();
-        int maxNorth = location/8, maxWest = location % 8;
-        int distance = Math.min(maxNorth, maxWest);
-        for(int i = 1; i <= distance; i++){
-            if(verifyColor(moves, color, location - (i*9), board.getSquare(location - (i*9)))){
-                break;
-            }
-        }
-        return moves;
-    }
-
-    public static ArrayList<Integer> getSouthEast(int location, String color, Board board){
-        ArrayList<Integer> moves = new ArrayList<Integer>();
-        int maxSouth = 7-(location/8), maxEast = 7 - (location % 8);
-        int distance = Math.min(maxSouth, maxEast);
-        for(int i = 1; i <= distance; i++){
-            if(verifyColor(moves, color, location + (i*9), board.getSquare(location + (i*9)))){
-                break;
-            }
-        }
-        return moves;
-    }
-
-    public static ArrayList<Integer> getSouthWest(int location, String color, Board board){
-        ArrayList<Integer> moves = new ArrayList<Integer>();
-        int maxSouth = 7-(location/8), maxWest = location % 8;
-        int distance = Math.min(maxSouth, maxWest);
-        for(int i = 1; i <= distance; i++){
-            System.out.println(i);
-            if(verifyColor(moves, color, location + (i*7), board.getSquare(location + (i*7)))){
-                break;
-            }
-        }
-        return moves;
-    }
-
+    
     public static boolean verifyColor(ArrayList<Integer> moves, String color, int location, Square sq){
         if(!(sq.hasPiece() && sq.getPiece().getColor().equals(color))){
             moves.add(location);
